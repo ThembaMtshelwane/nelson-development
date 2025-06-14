195158f62328c1a943e893f70dff0151c36f389a
ThembaMtshelwane <thembamm3@gmail.com>
2025-06-14 20:25:17 +0200

    Main Layout: Rounding the border

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
     <div className="h-screen bg-[#161B24] text-[#DDE9F1]">
       <div className="md:w-[640px] mx-auto h-[75%]">
         <Navbar />
-        <div className="h-full flex items-center justify-center border">
+        <div className="h-full flex items-center justify-center border rounded-2xl">
           <Outlet />
         </div>
       </div>
