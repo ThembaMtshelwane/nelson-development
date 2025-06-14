e4622626890abca77d5d63e82ebc3b91d4b2d560
ThembaMtshelwane <thembamm3@gmail.com>
2025-06-14 18:29:37 +0200

    Test Input form

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,43 @@
 const Test = () => {
-  return <div>Test</div>;
+  return (
+    <div className="h-full flex items-center justify-center">
+      <form
+        action=""
+        className="border p-2  w-full sm:w-[80%] flex flex-col gap-4 rounded-xl py-4"
+      >
+        <h2 className="text-center text-4xl font-bold">TEST</h2>
+        <div className="flex flex-col p-2 gap-2">
+          <label htmlFor="email" className="cursor-pointer">
+            Email:
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            required
+            className="border p-2 rounded-xl"
+            placeholder="Enter a word, e.g example"
+          />
+        </div>
+        <div className="flex flex-col p-2 gap-2">
+          <label htmlFor="apiURL" className="cursor-pointer">
+            API URL:
+          </label>
+          <input
+            id="apiURL"
+            name="apiURL"
+            type="text"
+            required
+            className="border p-2 rounded-xl"
+            placeholder="Neslson Development URL"
+          />
+        </div>
+        <button className="border w-fit mx-auto px-4 py-2 rounded-2xl hover:bg-[#DDE9F1] hover:text-[#161B24] cursor-pointer hover:scale-[1.01] font-bold">
+          SUBMIT
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Test;
