039282ea10745571b834d6c44cb6af348342c26d
ThembaMtshelwane <thembamm3@gmail.com>
2025-06-14 18:07:59 +0200

    Navbar and Layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="border flex justify-between p-4">
+      <h1>ND Test</h1>
+      <ul className="border flex w-[50%] sm:w-[30%] md:w-[200px] justify-around ">
+        <Link to="">Test</Link>
+        <Link to="play">Play</Link>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,9 +3,13 @@ import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
+    <div className="h-screen bg-[#161B24] text-[#DDE9F1]">
+      <div className="md:w-[880px] mx-auto h-[90%]">
+        <Navbar />
+        <div className="border h-full p-4">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
